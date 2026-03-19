78a4fa1d341b2c9ef8397da4641bb0f90a194845
Massimo Paladin <massimo.paladin@gmail.com>
2026-03-19 14:31:23 +0100

    Use macosx-aarch64 Sonar Scanner download URL

--- build-analyze.sh
+++ build-analyze.sh
@@ -18,9 +18,9 @@ unzip -o $HOME/.sonar/build-wrapper-macosx-x86.zip -d $HOME/.sonar/
 export PATH=$HOME/.sonar/build-wrapper-macosx-x86:$PATH
 
 # Download sonar-scanner
-curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-macosx.zip 
+curl -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-macosx-aarch64.zip 
 unzip -o $HOME/.sonar/sonar-scanner.zip -d $HOME/.sonar/
-export PATH=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-macosx/bin:$PATH
+export PATH=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-macosx-aarch64/bin:$PATH
 
 # Setup the build system
 xcodebuild -project macos-xcode.xcodeproj clean
